46445df4265dabcd9f5d12b8b9a0eb8788c43db3
cx098 <341283773@qq.com>
2025-01-24 14:44:59 +0800

    Add files via upload

---
+++ 版本信息.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import sklearn
+import joblib
+import scipy
+import matplotlib
+import tensorflow.keras as keras
+
+# 打印版本信息
+print("pandas version:", pd.__version__)
+print("numpy version:", np.__version__)
+print("tensorflow version:", tf.__version__)
+print("keras version:", keras.__version__)
+print("scikit-learn version:", sklearn.__version__)
+print("joblib version:", joblib.__version__)
+print("scipy version:", scipy.__version__)
+print("matplotlib version:", matplotlib.__version__)
